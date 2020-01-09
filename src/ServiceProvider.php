2f30041e744a878256c6130592399847b0bb83b3
李星星 <sss60@qq.com>
2020-01-09 08:52:58 +0800

    update

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Lee2son\DingTalkRobot;
 
-class ServiceProvider extends \Illuminate\Support\ServiceProvider
+use Illuminate\Contracts\Support\DeferrableProvider;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider implements DeferrableProvider
 {
     /**
      * Bootstrap any application services.
@@ -37,5 +39,16 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
         Robot::$default = $config['default'];
         Robot::$robots = $config['robots'];
+
+        $this->app->singleton('dingtalk.robot', function($name = null) {
+            return robot($name);
+        });
+    }
+
+    public function provides()
+    {
+        return [
+            'dingtalk.robot'
+        ];
     }
 }
